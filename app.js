5c2118751ecc8758a99fd35ff0aae09b11fe6fed
Chris Welch <deadclap893@gmail.com>
2017-07-28 07:24:21 -0400

    Main Komada #indev push!

---
+++ app.js
@@ -0,0 +1,11 @@
+const Komada = require('komada');
+
+const tomBot = new Komada.Client({
+  ownerID: "195223544186142727",
+  prefix: "t.",
+  clientOptions: {
+    fetchAllMembers: true,
+  },
+});
+
+tomBot.login(require('../bot_logins.json').Tom)
