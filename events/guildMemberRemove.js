7901d2fd661f958816d9b5fa575befc95f002537
Chris Welch <deadclap893@gmail.com>
2017-07-29 10:43:27 -0400

    [memberJoin / memberLeave] Added feature!

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,15 @@
+exports.run = (client, member) => {
+    var guild = member.guild
+    
+    if (!client.settingGateway.get(guild.id).joinLeaveLog) return
+
+
+        if (client.settingGateway.get(guild.id).joinLeaveLogEnabled === true) {
+        var channel = client.channels.get(client.settingGateway.get(guild.id).joinLeaveLog)
+    if (!channel.permissionsFor(client.user).has(["READ_MESSAGES","SEND_MESSAGES"], true)) return
+
+        channel.send(`:x: ${member.user.tag} has left the server! Total members: **${guild.members.size}**`)
+}
+
+
+}
