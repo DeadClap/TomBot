957a25b71229b3570f9a45c5409f586d7b1b03be
Chris Welch <deadclap893@gmail.com>
2017-07-29 10:42:40 -0400

    [guildCreate / guildDelete] Logging

---
+++ events/guildDelete.js
@@ -0,0 +1,23 @@
+exports.run = (client, guild) => {
+    function usersToBots (members) {
+        var u = 0
+        var b = 0
+
+        members.forEach(m => {
+            if (m.user.bot) b = b + 1
+            if (!m.user.bot) u = u + 1
+        })
+        return [u,b]
+    }
+
+    var uTb = usersToBots(guild.members)
+    var embed = new client.methods.Embed()
+    .setAuthor('Guild Left ' + guild.name)
+    .addField('Owner',`${guild.owner.user.tag} [${guild.owner.user.id}]`)
+    .addField('Users', uTb[0], true)
+    .addField('Bots', uTb[1], true)
+    .setColor(0x410074)
+    .setThumbnail(`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.jpg`)
+    
+    client.guilds.get('332178051154706432').channels.get('340864447037833226').send({embed})
+}
