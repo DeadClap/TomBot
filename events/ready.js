8a8cd6770d815d0ee4f345c7d1d9b9c098380f94
Chris Welch <deadclap893@gmail.com>
2017-08-02 06:33:05 -0400

    [ready] fixed rotation

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 exports.run = (client) => {
     var games = [`with ${client.users.size} users on ${client.guilds.size} servers`, `\@${client.user.tag} help`, `https://discord.gg/mDb2CBx`]
-    setTimeout(() => {
+    setInterval(() => {
     client.user.setGame(games[Math.floor(Math.random() * games.length)])
     }, 120000)
 }
