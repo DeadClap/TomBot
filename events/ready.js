dae8ff4ed32ef3827658637069de0aad10773c01
Chris Welch <deadclap893@gmail.com>
2017-08-02 04:12:23 -0400

    [ready] changed delay to 120 seconds]

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ exports.run = (client) => {
     var games = [`with ${client.users.size} users on ${client.guilds.size} servers`, `\@${client.user.tag} help`, `https://discord.gg/mDb2CBx`]
     setTimeout(() => {
     client.user.setGame(games[Math.floor(Math.random() * games.length)])
-    }, 10000)
+    }, 120000)
 }
