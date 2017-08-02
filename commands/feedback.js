e90f1d2d861da992033ef685edcd4f753e4eb677
Chris Welch <deadclap893@gmail.com>
2017-08-02 07:17:50 -0400

    Beautified

--- commands/feedback.js
+++ commands/feedback.js
@@ -1,31 +1,31 @@
-    const ghissues = require('ghissues')
+const ghissues = require('ghissues')
     , authOptions = {
-        user: 'DiscordTomBot', 
+        user: 'DiscordTomBot',
         token: 'f8e3aa5a1ab53dc021b7ebea37f0994f9e2d20e7'
     }
 exports.run = async (client, msg, [t, ...body]) => {
     var b = body.join(' ');
     if (t === "fReq" || t === "freq") {
-         var type = "feature"
+        var type = "feature"
     } else {
         var type = t
     }
-   
+
     var data = {
         title: "Feedback from " + msg.author.tag + " on " + msg.guild.name,
         body: b,
         labels: [client.funcs.toTitleCase(type), "Bot Sent"]
     }
-    ghissues.create(authOptions, 'DeadClap', 'TomBot', data , function(err, issue) {
+    ghissues.create(authOptions, 'DeadClap', 'TomBot', data, function (err, issue) {
         if (err) {
             client.emit("error", err)
         }
         console.log(issue)
 
         var embed = new client.methods.Embed()
-        .setTitle(data.title)
-        .setDescription(data.body)
-        return msg.channel.send('Thanks for your feedback!', {embed})
+            .setTitle(data.title)
+            .setDescription(data.body)
+        return msg.channel.send('Thanks for your feedback!', { embed })
     })
 
 
