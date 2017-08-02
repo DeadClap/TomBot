4a3a05a5262350a668ff01f94bb2d5314ccdac97
Chris Welch <deadclap893@gmail.com>
2017-08-02 06:49:51 -0400

    [gMA] Save as last commit

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,10 +1,10 @@
 exports.run = (client, member) => {
     var guild = member.guild
     // client.user.setGame(`with ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users on ${client.guilds.size} servers`)
-    if (!client.settingGateway.get(guild.id).joinLeaveLog) return
+    
 
 
-        if (client.settingGateway.get(guild.id).joinLeaveLogEnabled === true) {
+        if (client.settingGateway.get(guild.id).joinLeaveLog != null && client.settingGateway.get(guild.id).joinLeaveLogEnabled === true) {
         var channel = client.channels.get(client.settingGateway.get(guild.id).joinLeaveLog)
     if (!channel.permissionsFor(client.user).has(["READ_MESSAGES","SEND_MESSAGES"], true)) return
 
