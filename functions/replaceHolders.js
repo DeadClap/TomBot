2775b0de47998a140c4a8d705a5abaf807ebf21e
Chris Welch <deadclap893@gmail.com>
2017-08-02 08:26:17 -0400

    [replaceHolders] Maybe!

--- functions/replaceHolders.js
+++ functions/replaceHolders.js
@@ -1,4 +1,4 @@
-exports.run = (str, guild, member) => {
+module.exports = (str, guild, member) => {
     var out = new String()
     out = str.replace(/\%s/g, guild.name)
     out = out.replace(/\%m/g, guild.members.size)
