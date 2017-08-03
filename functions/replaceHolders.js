15d48a6e4d95c798753464a0cccaef8b4ebe6b66
Chris Welch <deadclap893@gmail.com>
2017-08-03 01:56:38 -0400

    [repHolders] Testing

--- functions/replaceHolders.js
+++ functions/replaceHolders.js
@@ -1,5 +1,6 @@
 module.exports = (str, guild, member) => {
     var out = new String()
+    console.log(str)
     out = str.replace(/\%s/g, guild.name)
     out = out.replace(/\%m/g, guild.members.size)
     out = out.replace(/\%um/g, member)
