13f7decf70a351656e02658909128839a99fe845
Chris Welch <deadclap893@gmail.com>
2017-08-04 10:05:56 -0400

    [replaceHolders] Disabled debug log

--- functions/replaceHolders.js
+++ functions/replaceHolders.js
@@ -1,11 +1,10 @@
 module.exports = (str, guild, member) => {
     var out = new String()
-    console.log(str)
+    // console.log(str)
     out = str.replace(/\%s/g, guild.name)
     out = out.replace(/\%m/g, guild.members.size)
     out = out.replace(/\%um/g, member)
     out = out.replace(/\%u/g, member.user.username)
-
 
     return out
 }
