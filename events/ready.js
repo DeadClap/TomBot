b23af5fdbdc4e2668e8c1ebe7fbdd5527c3fd22d
Chris Welch <deadclap893@gmail.com>
2017-08-04 10:18:18 -0400

    [ready] Attempted to fix rotation.

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,10 @@
 exports.run = (client) => {
     var games = [`with ${client.users.size} users on ${client.guilds.size} servers`, `\@${client.user.tag} help`, `https://discord.gg/mDb2CBx`]
-    setInterval(() => {
-        client.user.setGame(games[Math.floor(Math.random() * games.length)])
-    }, 120000)
+    for (game in games) {
+        client
+            .user
+            .setGame(game)
+        client.emit('log', game)
+        setTimeout(() => console.log('Changing Game...'), 120000)
+    }
 }
