742e642ff79f6bd63bafd95119fd0fb410ef6ba7
Christopher Welch <deadclap893@gmail.com>
2017-08-20 17:23:51 -0400

    [ready] Added a playing for startup bofore rotation start.

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,16 @@ function sleep(ms) {
 }
 
 exports.run = (client) => {
+    client.user.setPresence({
+        game: {
+            name: `\@${client.user.tag} help`,
+            type: 0
+        }
+    })
     var games = [`with ${client.users.size} users on ${client.guilds.size} servers`, `\@${client.user.tag} help`, `https://discord.gg/mDb2CBx`]
     var i = 0
     setInterval(() => {
-        
+        client.emit('log', 'Game: ' + client.user.presence.game.name + " " + i)
         client
             .user
             .setPresence({
@@ -19,6 +25,6 @@ exports.run = (client) => {
         i = i + 1
         if (i > games.length - 1) 
             i = 0
-        client.emit('log', 'Game: ' + client.user.presence.game.name + " " + i)
+        
     }, 160000)
 }
