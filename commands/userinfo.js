844ff7889963b3637eb4f33b5e2288bec946e763
Christopher Welch <deadclap893@gmail.com>
2017-09-06 05:31:06 -0400

    Please userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,22 +1,17 @@
-exports.run = async (client, msg, [user]) => {
-    
+exports.run = async(client, msg, [user]) => {
+
     if (!user) var user = msg.member;
-    if (user.roles.size > 0) { 
-        var roleString = user.roles.sort((a, b) => a.position - b.position || a.id - b.id).map(a => `${a.name}`).slice(1).reverse().join(', ')
-    } else if (user.roles.size === 0) {
-        var roleString = "None"
-    }
+    var roleString = user.roles.size >= 2 ? user.roles.sort((a, b) => a.position - b.position || a.id - b.id).map(a => `${a.name}`).slice(1).reverse().join(', ') : 'None'
 
 
-    
+
     var embed = new client.methods.Embed()
-    .addField('Full user', `${user.user.tag} (${user.id})`)
-    // if (user.roles.size > 1) { 
-    //     embed.addField('Roles', user.roles.sort((a, b) => a.position - b.position || a.id - b.id).map(a => `${a.name}`).slice(1).reverse().join(', '));
-    // } else {
-    //     embed.addField('Roles', 'None')
-    // }
-    return msg.channel.send({embed, disableEveryone: true});
+        .addField('Full user', `${user.user.tag} (${user.id})`)
+        .addField('Roles', roleString)
+    return msg.channel.send({
+        embed,
+        disableEveryone: true
+    });
 };
 
 exports.conf = {
@@ -24,7 +19,7 @@ exports.conf = {
     runIn: ["text"],
     aliases: ["ui", "user"],
     permLevel: 0,
-    botPerms: ["SEND_MESSAGES","EMBED_LINKS"],
+    botPerms: ["SEND_MESSAGES", "EMBED_LINKS"],
 };
 
 exports.help = {
