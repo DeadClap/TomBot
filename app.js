5dd8152f4c71b991ae26680063aad6a28605ef8c
Christopher Welch <DeadClap@users.noreply.github.com>
2017-09-06 11:12:45 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const Komada = require('komada');
+
+const tomBot = new Komada.Client({
+  ownerID: "195223544186142727",
+  prefix: "t.",
+  clientOptions: {
+    fetchAllMembers: true,
+  },
+  provider: { engine: "json" },
+  cmdLogging: true,
+  extraCFG: require('./extraCfg.json')
+});
+
+tomBot.login(require('../bot_logins.json').Tom)
