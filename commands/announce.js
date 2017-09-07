21ca73c74fe5351c76c83f1cafd7555cebf7f03f
Chris Welch <deadclap893@gmail.com>
2017-09-07 12:56:06 +0000

    Just testing

--- commands/announce.js
+++ commands/announce.js
@@ -1,5 +1,15 @@
 exports.run = (client, msg, [...ann]) => {
-    return msg.channel.send(ann.join(' '))
+    client.guilds.forEach(g => {
+        if (g.id !== '332178051154706432') return
+        if (!g.settings.announceChannel) {
+            g.owner.send(`You are recieving this because you don't have a guild announcement channel setup in your server.\n ${ann}`)
+        } else {
+            g.settings.announceChannel.send(ann.join)
+        }
+        
+    })
+    
+    // return msg.channel.send(ann.join(' '))
 }
 
 exports.conf = {
