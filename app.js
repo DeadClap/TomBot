e0970e1ff652b5c32322a21e804bb92d9f725f7c
Chris Welch <deadclap893@gmail.com>
2017-09-07 12:26:05 +0000

    Please

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const Komada = require('komada');
-
+var permStructure = new PermLevels()
+  .addLevel(0, false, () => true)
+  .addLevel(2, false, (client, msg) => {
+    if (!msg.guild || !msg.guild.settings.modRole) return false;
+    const modRole = msg.guild.roles.get(msg.guild.settings.modRole);
+    return modRole && msg.member.roles.has(modRole.id);
+  })
+  .addLevel(3, false, (client, msg) => {
+    if (!msg.guild || !msg.guild.settings.adminRole) return false;
+    const adminRole = msg.guild.roles.get(msg.guild.settings.adminRole);
+    return adminRole && msg.member.roles.has(adminRole.id);
+  })
+  .addLevel(4, false, (client, msg) => msg.guild && msg.author.id === msg.guild.owner.id)
+  .addLevel(8, false, (client, msg) => client.config.extraCFG.owners.includes(msg.author.id))
+  .addLevel(9, true, (client, msg) => msg.author.id === client.config.ownerID)
+  .addLevel(10, false, (client, msg) => msg.author.id === client.config.ownerID);
 const tomBot = new Komada.Client({
   ownerID: "195223544186142727",
   prefix: "t.",
   clientOptions: {
     fetchAllMembers: true,
   },
+  permStructure,
   provider: { engine: "json" },
   cmdLogging: true,
   extraCFG: require('./extraCfg.json')
 });
-
-tomBot.permStructure.addLevel(8, false, (client, msg) => client.config.extraCFG.owners.includes(msg.author.id));
 
 tomBot.login(require('../bot_logins.json').Tom)
