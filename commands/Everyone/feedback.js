4f612e07f35de1e074acf94ca0d5f6830e219384
Chris Welch <deadclap893@gmail.com>
2017-09-08 13:00:47 +0000

    Come on

--- commands/Everyone/feedback.js
+++ commands/Everyone/feedback.js
@@ -1,7 +1,7 @@
 const ghissues = require('ghissues')
     , authOptions = {
         user: 'DiscordTomBot',
-        token: require('../bot_logins.js').tomGithub
+        token: require('../../bot_logins.js').tomGithub
     }
 exports.run = async (client, msg, [t, ...body]) => {
     var myserver = client.guilds.get('332178051154706432')
