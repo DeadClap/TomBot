f654a76919573867cfd5e8f53fc963b8536e2314
Christopher Welch <deadclap893@gmail.com>
2017-09-09 15:51:25 -0400

    lets go

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const Komada = require('komada');
+const {Client, PermLevels} = require('komada');
 
-const tomBot = new Komada.Client({
+var permStructure = new PermLevels()
+.addLevel(0, false, () => true)
+.addLevel(2, false, (client, msg) => {
+  if (!msg.guild || !msg.guild.settings.modRole) return false;
+  const modRole = msg.guild.roles.get(msg.guild.settings.modRole);
+  return modRole && msg.member.roles.has(modRole.id);
+})
+.addLevel(3, false, (client, msg) => {
+  if (!msg.guild || !msg.guild.settings.adminRole) return false;
+  const adminRole = msg.guild.roles.get(msg.guild.settings.adminRole);
+  return adminRole && msg.member.roles.has(adminRole.id);
+})
+.addLevel(4, false, (client, msg) => msg.guild && msg.author.id === msg.guild.owner.id)
+.addLevel(8, false, (client, msg) => client.extraCFG.owners.includes(msg.author.id))
+.addLevel(9, true, (client, msg) => msg.author.id === client.config.ownerID)
+.addLevel(10, false, (client, msg) => msg.author.id === client.config.ownerID);
+
+
+const tomBot = new Client({
   ownerID: "195223544186142727",
   prefix: "t.",
   clientOptions: {
