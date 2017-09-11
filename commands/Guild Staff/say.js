f4d4b4be14d47a73255b63a829c9068f0d9cd4ec
Chris Welch <deadclap893@gmail.com>
2017-09-11 13:47:53 +0000

    Updated say.js

--- commands/Guild Staff/say.js
+++ commands/Guild Staff/say.js
@@ -1,8 +1,8 @@
 exports.run = async (client, msg, [channel, ...say]) => {
   var chan = channel ? channel : msg.channel
   
-  if (!chan.permissionsFor(client.user).has(19456)) return msg.reply(`\`\`\`Error: Missing Permissions in Channel [SEND_MESSAGES, READ_MESSAGES, EMBED_LINKS]\`\`\``)
-  return chan.send(say.join(' '))
+  if (!chan.permissionsFor(client.user).has(19456)) return msg.reply(`\`\`\`Error: Missing Permissions in Channel [SEND_MESSAGES, READ_MESSAGES, EMBED_LINKS]\`\`\``) && msg.delete(30000)
+  return chan.send(say.join(' ')) && msg.reply('Message Sent!') && msg.delete(30000)
 }
 
 exports.conf = {
