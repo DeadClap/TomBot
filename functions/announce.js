be3181cd15d40721453434585d3887740fded46c
Chris Welch <deadclap893@gmail.com>
2017-09-11 16:19:45 +0000

    Updated announce.js

--- functions/announce.js
+++ functions/announce.js
@@ -2,10 +2,10 @@ module.exports = (client, guild, announcement) => {
     var ann = announcement.join(' ')
     if (guild.settings.announceChannel) {
         var channel = guild.channels.get(guild.settings.announceChannel)
-        console.log(`[CHANNEL: ${channel}`)
+        console.log(`[CHANNEL: ${channel.name}`)
         return channel.send(ann)
     } else if (!guild.settings.announceChannel){
-        console.log(`[DM: ${guild.owner.username}]`)
+        console.log(`[DM: ${guild.owner.user.username}/${guild.owner.nickname}]`)
         return guild.owner.send(`[${guild.name}] You are recieving this because you own the guild prefixed and it does not have an announceChannel set in the config. Use \`\`\`t.conf set announceChannel \#channel\`\`\`\n${ann}`)
     }
 }
