9cf9e57363cc380d615c4794133e6e5a97bbb86f
Chris Welch <deadclap893@gmail.com>
2017-09-14 14:47:19 +0000

    Edited info.js

--- commands/Everyone/info.js
+++ commands/Everyone/info.js
@@ -1,10 +1,12 @@
 exports.run = async (client, msg) => {
 
     var prefix = msg.guild.settings.prefix ? msg.guild.settings.prefix : client.config.prefix
+    
     const information = [
         "TomBot is a general use, utility bot.",
         `Want to invite me? Use the \`${prefix}invite\``,
-        `Have any suggestions, bugs, or feedback please use \`${prefix}feedback\``
+        `Have any suggestions, bugs, or feedback please use \`${prefix}feedback\``,
+        `Need some support? Join the TomBot Server: [Click here](https://discord.gg/mDb2CBx)`
 
     ]
 
