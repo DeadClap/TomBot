7c3bf482e0d11f87081a4bab6b547988d1682c21
Chris Welch <deadclap893@gmail.com>
2017-09-18 13:09:50 +0000

    Updated warn.js

--- commands/Guild Staff/warn.js
+++ commands/Guild Staff/warn.js
@@ -13,8 +13,8 @@ exports.conf = {
 };
 
 exports.help = {
-    name: 'base', // name of the command
-    description: "Basis of all commands in this bot", // Description of the command for the help command
+    name: 'warn', // name of the command
+    description: "Warns a user of breaking a rule or guideline", // Description of the command for the help command
     usage: "<user:mention> <reason:str> [...]", // as mentioned above this is your usage string. e example 1: "<user:mention> <reason:str> [...]" in the (client, msg, [user,...reason])
     usageDelim: " " // This is what to split your arguments by, can be a space, comma or anything you want.
 };
