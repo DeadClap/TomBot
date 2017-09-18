998dd673fcdb6d66e453bd740da4814e38983fe3
Chris Welch <deadclap893@gmail.com>
2017-09-18 13:07:36 +0000

    Added warn.js

---
+++ commands/Guild Staff/warn.js
@@ -0,0 +1,20 @@
+exports.run = async /*Don't forget async*/ (client, msg, [user, ...reason] /* if you add anything to help.usage add the names here. in an Array, if you have a [...] after an arg use [arg1,arg2,...arg3] if you third arg is more than one word.*/) => {
+    msg.reply('Indev')
+};
+
+exports.conf = {
+    enabled: true, // Self Explainatory - Ignore my spelling
+    runIn: ['text','dm','group'], // Channel Types this command can run
+    aliases: [], // Other commands that trigger this one
+    permLevel: 2, // Minumum Perms required to run this command
+    requireFuncs: [], // Funcs required for this to work
+    requiredSettings: ["modlogs"]  // Guild Settings required for this to run
+    //cooldown: 0  // uncomment the first bit to use cooldown function
+};
+
+exports.help = {
+    name: 'base', // name of the command
+    description: "Basis of all commands in this bot", // Description of the command for the help command
+    usage: "<user:mention> <reason:str> [...]", // as mentioned above this is your usage string. e example 1: "<user:mention> <reason:str> [...]" in the (client, msg, [user,...reason])
+    usageDelim: " " // This is what to split your arguments by, can be a space, comma or anything you want.
+};
