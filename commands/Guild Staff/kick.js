23c1cee4eff032eeac9be4b8cd6c8e21f72f6150
Chris Welch <deadclap893@gmail.com>
2017-09-19 16:55:47 +0000

    Updated kick.js

--- commands/Guild Staff/kick.js
+++ commands/Guild Staff/kick.js
@@ -1,4 +1,4 @@
-exports.run = async(client,msg, [user, ...reason]) => {
+exports.run = async(client,msg, [dry ,user, ...reason]) => {
     if (!client.funcs.many.kickable(client,msg, msg.member, user, msg.guild)) return msg.channel.send('Sorry but you can not kick this person!',{reply:msg.author.id})
     return msg.channel.send(`${user.user.tag} has been kicked sucessfully!`)
 }
@@ -12,6 +12,6 @@ exports.conf = {
 exports.help = {
     name: "kick",
     description: "Allows you no kick a guild member",
-    usage: "<user:member> [reason:str] [...]",
+    usage: "[-t:literal] <user:member> [reason:str] [...]",
     usageDelim: " "
 };
