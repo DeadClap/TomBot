1c2b95699688340d31200d718d69e288f60a4097
Chris Welch <deadclap893@gmail.com>
2017-09-19 13:22:29 +0000

    Updated warn.js

--- commands/Guild Staff/warn.js
+++ commands/Guild Staff/warn.js
@@ -1,33 +1,22 @@
-exports.run = async(client, msg, [user, ...reason]) => {
+exports.run = async (client, msg, [user, ...reason]) => {
     var embed = new client.methods.Embed()
         .setColor(client.funcs.hex())
         .setTitle('Warning')
         .addField('Member', user.tag)
         .addField('Reason', reason.join(' '))
-        .setFooter(`Acting Moderator: ` + msg.author.tag, msg.author.displayAvatarURL({ format: 'jpg', size: 256 }))
+        .setFooter(`Acting Moderator: ` + msg.author.tag, msg.author.displayAvatarURL({format: 'jpg', size: 256}))
     var embed2 = new client.methods.Embed()
         .setColor(client.funcs.hex())
         .setTitle('Warning in ' + msg.guild.name)
         .addField('Reason', reason.join(' '))
-        .setFooter(`Acting Moderator: ` + msg.author.tag, msg.author.displayAvatarURL({ format: 'jpg', size: 256 }))
-
+        .setFooter(`Acting Moderator: ` + msg.author.tag, msg.author.displayAvatarURL({format: 'jpg', size: 256}))
+    var embed3 = new client.methods.Embed()
+        .setDescription(`${user.tag} has been successfully warned!`)
     if (msg.guild.settings.modlogs) {
-        user.send({ embed: embed2 }).catch(e => msg.reply(`User has dm's disabled.`))
-        msg.guild.channels.get(msg.guild.settings.modlogs).send({ embed }).catch(e => msg.reply(`Insufficient modlogs permissions.`))
-        return msg.delete() && msg.channel.send({
-            embed: {
-                "embed": {
-                    "fields": [{
-                            "name": "Status:",
-                            "value": "Warn Sucessful"
-                        }
-
-                    ]
-                }
-            }
-        })
-    }
-    else return msg.channel.send(`Failed: Guild doesn't have a modlogs set in the config.`)
+        user.send({embed: embed2}).catch(e => msg.reply(`User has dm's disabled.`))
+        msg.guild.channels.get(msg.guild.settings.modlogs).send({embed}).catch(e => msg.reply(`Insufficient modlogs permissions.`))
+        return msg.delete() && msg.channel.send({embed: embed3})
+    } else return msg.channel.send(`Failed: Guild doesn't have a modlogs set in the config.`)
 }
 
 exports.conf = {
