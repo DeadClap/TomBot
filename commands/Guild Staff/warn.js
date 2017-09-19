00296588eba67f4df17524d269a4d7b73afa243a
Chris Welch <deadclap893@gmail.com>
2017-09-19 13:03:21 +0000

    Updated warn.js

--- commands/Guild Staff/warn.js
+++ commands/Guild Staff/warn.js
@@ -2,7 +2,7 @@ exports.run = async(client, msg, [user, ...reason]) => {
     var embed = new client.methods.Embed()
         .setTitle('Warning')
         .addField('Member', user.tag)
-        .addField('Reason', reason)
+        .addField('Reason', reason.join(' '))
         .setFooter(msg.author.tag, msg.author.displayAvatarURL({format: 'jpg', size: 256}))
     if (msg.guild.settings.modlogs) {
         return msg.guild.channels.get(msg.guild.settings.modlogs).send({embed}).catch(e => msg.reply(`Insufficient Permissions in modlogs`))
