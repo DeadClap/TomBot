9a05441f079d48506b32b32291af72b320393dc6
Chris Welch <deadclap893@gmail.com>
2017-09-19 16:35:23 +0000

    Edited kickable function

--- functions/many.js
+++ functions/many.js
@@ -2,6 +2,6 @@ exports.kickable = (client, msg, user1, user2, guild) => {
     var role1 = user1.highestRole;
     var role2 = user2.highestRole
     
-   if (user2.id === guild.ownerID) return false
+   if (user2.id === guild.owner.id) return false
    return role1.position > role2.postition
 }
