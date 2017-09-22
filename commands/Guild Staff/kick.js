8aff7d4e32cb91fc18491778392b7eeccfb1a34f
Christopher Welch <deadclap893@gmail.com>
2017-09-22 07:23:59 -0400

    Needed commit

--- commands/Guild Staff/kick.js
+++ commands/Guild Staff/kick.js
@@ -1,14 +1,32 @@
 exports.run = async(client,msg, [user, ...reason]) => {
     
-    
+    var modmsg = new client.methods.Embed()
+    .setAuthor('Kicked: ' + user.user.tag)
+    .addField('Reason', reason.join(' '))
+    .setTimestamp()
+    .setFooter(`Acting Moderator: ${msg.author.tag}`, msg.author.displayAvatarURL({format: 'png', size: 256}))
+    .setThumbnail(user.user.displayAvatarURL({format: 'png', size: 256}))
+
+    var umsg = new client.methods.Embed()
+    .setAuthor('You were kicked from: ' + msg.guild.name)
+    .addField('Reason', reason.join(' '))
+    .setTimestamp()
+    .setFooter(`By ${msg.author.tag}`, msg.author.displayAvatarURL({format: 'png', size: 256}))
+    .setThumbnail(user.user.displayAvatarURL({format: 'png', size: 256}))
+
+    var smsg = new client.methods.Embed()
+    .setAuthor('Kicked: ' + user.user.tag)
+    .setTimestamp()
+
     
     var check = client.funcs.many.kickable(client,msg,msg.member,user,msg.guild)    
-    if (check === "Error: Bot Missing Kick Permission") return msg.channel.send('I do not have permission to users from this server!', {reply: msg.author.id})
     if (check === "Error: User Missing Kick Permission") return msg.channel.send('You do not have permission to users from this server!', {reply: msg.author.id})
+    if (check === "Error: Bot Missing Kick Permission") return msg.channel.send('I do not have permission to users from this server!', {reply: msg.author.id})
     if (check === "Error: Guild Owner") return msg.channel.send('You cannot kick the guild owner!', {reply: msg.author.id})
-    if (check === "Error: Bot Role Lower Than Target") return msg.channel.send('I have permission, but I am not above the user!', {reply: msg.author.id})
     if (check === "Error: Self Target") return msg.channel.send('You cannot kick yourself!', {reply: msg.author.id})
-    if (check === true) return 
+    if (check === "Error: Bot Role Lower Than Target") return msg.channel.send('I have permission, but I am not above the user!', {reply: msg.author.id})
+    if (check === false) return msg.channel.send('You have permission, but you are not above the user!', {reply: msg.author.id})
+    if (check === true) return msg.channel.send({embed: smsg})
     
 }
 exports.conf = {
