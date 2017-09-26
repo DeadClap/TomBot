bc5dd6e5aa3455523fd27ddc0223c2153db84882
Chris Welch <deadclap893@gmail.com>
2017-09-26 13:09:14 +0000

    Updated invite.js

--- commands/Everyone/invite.js
+++ commands/Everyone/invite.js
@@ -2,7 +2,7 @@ exports.run = async(client, msg) => {
     var m = [
             "So you want to invite me?",
             "Glad to hear it. Hope you enjoy my services.",
-            `[Click here](https://discordapp.com/oauth2/authorize/?permissions=2146958591&scope=bot&client_id=333839071115739137) to invite me!`
+            `[Click here](https://discordapp.com/oauth2/authorize/?permissions=2146958591&scope=bot&client_id=${client.user.id}) to invite me!`
         ]
     var embed = new client.methods.Embed()
         .setTitle('TomBot invitation')
