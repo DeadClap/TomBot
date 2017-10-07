6501be6c9c577dac38d6025db74870c8f5df5ef6
Christopher <deadclap893@gmail.com>
2017-10-07 11:53:43 -0400

    Updated many.js (Kickable Function)

--- functions/many.js
+++ functions/many.js
@@ -1,26 +1,15 @@
 exports.kickable = (client, msg, user1, user2, guild) => {
-   if (!client.funcs.clientHasPerm.guildMember(client, guild,2)) return "Error: Bot Missing Kick Permission"
-   if (!user1.hasPermission(2)) return "Error: User Missing Kick Permission"
+   if (!client.funcs.clientHasPerm.guildMember(client, guild,2)) msg.channel.send('I do not have permission kick to users from this server!', {reply: msg.author.id})
+   if (!user1.hasPermission(2)) return msg.channel.send('You do not have permission to users from this server!', {reply: msg.author.id})
    var pos1 = user1.highestRole.position
    var pos2 = user2.highestRole.position
    
    if (user1.id === guild.owner.id) pos1 = 1000
-   if (user2.id === client.user.id) return "Error: Bot Self Target"
-   if (user1.id === user2.id) return "Error: Self Target"
-   if (user2.id === guild.owner.id) return "Error: Guild Owner"
-   if (guild.member(client.user).highestRole.position === pos2) return "Error: Bot Role Equal With Target"
-   if (guild.member(client.user).highestRole.position < pos2) return "Error: Bot Role Lower Than Target"
+   if (user2.id === client.user.id) return msg.channel.send('I cannot kick myself!', {reply: msg.author.id})
+   if (user1.id === user2.id) return msg.channel.send('You cannot kick yourself!', {reply: msg.author.id})
+   if (user2.id === guild.owner.id) return msg.channel.send('You cannot kick the guild owner!', {reply: msg.author.id})
+   if (guild.member(client.user).highestRole.position <= pos2) return msg.channel.send('I have permission, but I am not above the user!', {reply: msg.author.id})
    return pos1 > pos2
 
-}
-
-exports.addCase = (client, guild, offender, title, reason) => {
-   var channel = guild.settings.modlogs ? guild.channels.get(guild.settings.modlogs) : null
-   if (channel === null) return "Error: Channel Null"
-   var embed = client.methods.Embed()
-   .addField('Action', title)
-   .addField('Offender', offender.user.tag)
-   .addField('Offence', reason.join(' '))
-   return channel.send(embed)
 }
 
