4642285aa76771a1fd7e391739a824e99e4ce9bc
Christopher <deadclap893@gmail.com>
2017-10-07 11:53:24 -0400

    Added DM Logger

---
+++ monitors/logger.js
@@ -0,0 +1,23 @@
+exports.conf = {
+    enabled: true,
+    ignoreBots: true,
+    ignoreSelf: true
+}
+
+exports.run = (client, msg, cmd) => {
+    if (msg.channel.type !== 'dm') return;
+    var channel = client.channels.get('363829630207131650');
+    var commands = client.commands.keyArray()
+    var aliases = client.aliases.keyArray()
+    var wordArray = msg.content.split(' ');
+    var one = wordArray[0].replace(client.settings.guilds.schema.prefix.default, '')
+    if (commands.includes(one) || aliases.includes(one)) return
+    client.emit('log', msg.author.tag + ' DMed: ' + msg.cleanContent)
+
+    var embed = new client.methods.Embed()
+    .setAuthor(msg.author.tag + ` (${msg.author.id})`, msg.author.displayAvatarURL({format: 'png', size: 256}))
+    .setDescription(msg.cleanContent)
+    
+
+    channel.send({embed})
+}
