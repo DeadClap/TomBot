b6b89a7ee20c279797cad0b9b9198127319900d2
Christopher <deadclap893@gmail.com>
2017-10-08 16:22:34 -0400

    Updated dm.js

--- commands/Owner/dm.js
+++ commands/Owner/dm.js
@@ -1,7 +1,7 @@
 exports.run = async (client, msg, [user, ...message]) => {
   var channel = client.channels.get('363809453260144650')
-  if (msg.author.id === client.owner.id) return user.send('\*\*DEV\*\* ' + msg.author.tag + ':\n' + message.join(' ')) && msg.channel.send('\*\*DEV\*\* ' + msg.author.tag + 'to ' + user.tag +':\n' + message.join(' ')) && msg.delete() && channel.send('\*\*DEV\*\* ' + msg.author.tag + ' to ' + user.tag +':\n' + message.join(' '))
-  if (client.config.extraCFG.owners.includes(msg.author.id)) return user.send('\*\*OWNER\*\* ' + msg.author.tag + ':\n' + message.join(' ')) && msg.channel.send('\*\*OWNER\*\* ' + msg.author.tag + ' to ' + user.tag +':\n' + message.join(' ')) && msg.delete() && channel.send('\*\*OWNER\*\* ' + msg.author.tag + 'to ' + user.tag +':\n' + message.join(' '))
+  if (msg.author.id === client.owner.id) return user.send('\*\*DEV\*\* ' + msg.author.tag + ':\n' + message.join(' ')) && msg.channel.send('\*\*DEV\*\* ' + msg.author.tag + ' to ' + user.tag +':\n' + message.join(' ')) && msg.delete() && channel.send('\*\*DEV\*\* ' + msg.author.tag + ' to ' + user.tag +':\n' + message.join(' '))
+  if (client.config.extraCFG.owners.includes(msg.author.id)) return user.send('\*\*OWNER\*\* ' + msg.author.tag + ':\n' + message.join(' ')) && msg.channel.send('\*\*OWNER\*\* ' + msg.author.tag + ' to ' + user.tag +':\n' + message.join(' ')) && msg.delete() && channel.send('\*\*OWNER\*\* ' + msg.author.tag + ' to ' + user.tag +':\n' + message.join(' '))
 }
 
 
