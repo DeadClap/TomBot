1365bb273364161764aa64d3a030db562fdcf566
Chris Welch <deadclap893@gmail.com>
2017-10-09 19:02:49 +0000

    Updated logger.js

--- monitors/logger.js
+++ monitors/logger.js
@@ -1,3 +1,4 @@
+
 exports.conf = {
     enabled: true,
     ignoreBots: true,
@@ -5,6 +6,13 @@ exports.conf = {
 }
 
 exports.run = (client, msg, cmd) => {
+    
+    var disabledEmbed = new client.methods.Embed()
+    .setTitle('Direct Message Logging Desabled')
+    .setDescription('Please Select from one of these.')
+    .addField('Help with EverWing Hacks by Tomas', `[Click here](https://discord.gg/93UhENE)`)
+    .addField('Help wit TomBot', `[Click here](https://discord.gg/mDb2CBx)`)
+    
     if (msg.channel.type !== 'dm') return;
     
     if (client.config.beta) {
@@ -14,17 +22,17 @@ exports.run = (client, msg, cmd) => {
     var aliases = client.aliases.keyArray()
     var wordArray = msg.content.split(' ');
     var one = wordArray[0].replace(client.settings.guilds.schema.prefix.default, '')
-    // if (commands.includes(one) || aliases.includes(one)) return
-    if (!commands.includes(one) || !aliases.includes(one)) return msg.reply('DM LOGGING disabled')
+    if (commands.includes(one) || aliases.includes(one)) return
+    return msg.reply('DM Logging disabled!')
 
-    var embed = new client.methods.Embed()
-    .setAuthor(msg.author.tag + ` (${msg.author.id}) sent:`, msg.author.displayAvatarURL({format: 'png', size: 256}))
-    .setDescription(msg.cleanContent)
+    // var embed = new client.methods.Embed()
+    // .setAuthor(msg.author.tag + ` (${msg.author.id}) sent:`, msg.author.displayAvatarURL({format: 'png', size: 256}))
+    // .setDescription(msg.cleanContent)
     
-    if (msg.attachments.size > 0) {msg.attachments.forEach(a => {
-        channel.sendFile(a.url)
-        client.emit('log', msg.author.tag + ' DMed: ' + msg.cleanContent + ' (WITH IMAGE(s))')
-    })} else client.emit('log', msg.author.tag + ' DMed: ' + msg.cleanContent)
-    channel.send({embed})
+    // if (msg.attachments.size > 0) {msg.attachments.forEach(a => {
+    //     channel.sendFile(a.url)
+    //     client.emit('log', msg.author.tag + ' DMed: ' + msg.cleanContent + ' (WITH IMAGE(s))')
+    // })} else client.emit('log', msg.author.tag + ' DMed: ' + msg.cleanContent)
+    // channel.send({embed})
     
 }
